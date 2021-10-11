e1b6f2f2fbda5a3f9ca3c839f67444cdb88b431f
alfonsosantacruz <asantacruz@minerva.kgi.edu>
2021-10-11 12:01:16 -0600

    First commit from cookiecutter

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Machine Learning model to detect COVID19 out of chest x-ray images',
+    author='Alfonso Santacruz Garcia',
+    license='MIT',
+)
